eb499796f5538db2d83359abdd1b8e990dcdae5f
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2024-12-09 16:59:23 +0530

    Add files via upload

---
+++ 1.basics/1_variables.js
@@ -0,0 +1,20 @@
+const accountid = 15477
+let accountemail = "pk@google.com"
+var accountpass = "1234"
+accountcity ="jaipur"
+let accountstate 
+
+/* accountid = 2 
+not allowed because 
+it is constant that can be defined only once
+*/
+
+accountemail = "pk@gc.com"
+accountpass = "158752"
+accountcity = "bengluru"
+
+/*
+Do not prefer var 
+because of issue in block scope and function scope
+*/
+console.table([accountid,accountemail,accountpass,accountcity,accountsate]);
---
+++ 1.basics/3_conversions.js
@@ -0,0 +1,43 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+let str1 = "pk" 
+let str2 = "kkk"
+ 
+let str3 = str1 + str2
+//console.log(str3);
+
+
+
