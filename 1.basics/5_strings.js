5dc3fe5cf2f4a30f419b8ef6edb945585fc83cb2
Prathamesh Kurekar <146461949+Prathamesshh@users.noreply.github.com>
2024-12-10 15:44:53 +0530

    Add files via upload

---
+++ 1.basics/5_strings.js
@@ -0,0 +1,39 @@
+const name="yash"
+const repoCount = 10
+
+//console.log(name + repoCount + "value");
+
+/*concatinating a string best way
+ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+*/
+
+// declaring string command
+const gameName = new String("Yash")
+
+// console.log(gameName[0]);
+
+//  console.log(gameName.length);
+
+//  console.log(gameName.toUpperCase());
+
+//  console.log(gameName.charAt(2));
+
+const newstr = gameName.substring(0,3)
+
+//   console.log(newstr);
+
+
+const newstr1 = "  yash   "
+console.log(newstr1.length);
+
+console.log(newstr1.trim());
+
+
+const nname = "yash-ph-Pk"
+
+// console.log(nname.split('-'));
+
+
+
+
+
---
+++ 1.basics/6_maths.js
@@ -0,0 +1,28 @@
+/*
+const balance = new Number(100)
+console.log(balance.toFixed(2));
+
+
+const num = 13.4567
+console.log(num.toPrecision(3));
+
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-CA'));
+*/
+
+/*
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.78));
+console.log(Math.sqrt(3.78));
+*/
+
+console.log(Math.random());
+// Math.random gives you value between 0 and 1
+console.log((Math.random()*10) +1);
+
+
+
+
+
