ef2e673157300b68046eaff7390a4edccbd59229
Prathamesshh <prathameshkurekar@gmail.com>
2024-12-11 16:09:51 +0530

    changes

---
+++ 2.basics/1_arrays.js
@@ -0,0 +1,31 @@
+const arr =  [0,1,2,3,4]
+// console.log(arr[0]);
+
+// Array Methods
+/*
+arr.push(6)
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+*/
+
+// arr.unshift(9)
+// console.log((arr));
+
+
+// console.log(arr.includes(9))
+
+// console.log(arr.indexOf(4));
+
+// slice and splice
+/* slice returns the value from index values given but 
+splice gives the value and cuts the array into 2 parts
+*/
+
+const myn1 = arr.slice(1,3)
+console.log(myn1);
+
+const myn2 = arr.splice(1,3)
+console.log(myn2);
