af17151a0a5dc761bfc3c96aec1077367daf8373
Prathamesshh <prathameshkurekar@gmail.com>
2024-12-12 16:40:59 +0530

    commit

---
+++ 3.basics/1_functions.js
@@ -0,0 +1,14 @@
+function my_name(){
+    console.log("Yash");
+    
+}
+// my_name()
+
+/*function addTwoNumbers(number1 , number2){
+    console.log(number1+number2);
+       
+}
+    */
+
+
+function
