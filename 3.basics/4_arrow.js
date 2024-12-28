24a5cb018fea6d3bb73aa26aa03295abdf0700e7
Prathamesshh <prathameshkurekar@gmail.com>
2024-12-28 18:54:19 +0530

    push

---
+++ 3.basics/4_arrow.js
@@ -0,0 +1,29 @@
+/*
+const chai = function() {
+  let username = "John";
+  console.log(this.username);
+};
+
+chai(); 
+*/
+
+/*
+const chai = () => {
+    let username = "John";
+    console.log(this.username);
+  };
+  
+  chai(); 
+
+*/
+
+const add = (num1,num2) =>{
+return num1 + num2;
+}
+
+console.log(add(2,3)); // 5
+
+/*
+2nd method 
+const add = (num1,num2) => num1 + num2;
+*/
